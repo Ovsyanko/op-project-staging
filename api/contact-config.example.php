bf1675836a48833f6849619f923c6bd43b2748d1
Ovsyanko <anton.ovsyanko@gmail.com>
2026-09-15 23:56:05 +0300

    Add SMTP config template

---
+++ api/contact-config.example.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'smtp_host' => 'mail.hosting.reg.ru',
+    'smtp_port' => 465,
+    'smtp_user' => 'webform@op-project.ru',
+    'smtp_password' => 'PASTE_PASSWORD_HERE',
+    'from_email' => 'webform@op-project.ru',
+    'from_name' => 'OP Project website',
+    'to_email' => 'sales@op-project.ru',
+    'to_name' => 'OP Project Sales',
+];
